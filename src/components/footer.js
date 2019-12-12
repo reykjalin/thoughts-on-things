1c7203261cfb42b4a2651b48ce3cccfd63563199
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-12-13 06:33:51 +0800

    Fix footer code

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,20 +1,23 @@
 import React from 'react'
 
 import './footer.css';
-import { Link } from 'gatsby';
 
 export const Footer = () => {
     return (
 		<footer className={ 'site-footer' }>
 			<hr className="no-slash" />
-			<small>
-					Made with ♥ for the web | <a href="/rss.xml">rss</a>
-					<br />
-					This website does not track you or collect analytics.
-			</small>
-			<small>
-					© 2019 thoughts on things
-			</small>
+			<p>
+				<small>
+						Made with ♥ for the web | <a href="/rss.xml">rss</a>
+						<br />
+						This website does not track you or collect analytics.
+				</small>
+			</p>
+			<p>
+				<small>
+						© 2019 thoughts on things
+				</small>
+			</p>
 		</footer>
     )
 }
