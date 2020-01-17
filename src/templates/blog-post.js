bdc0d5aaefcc1e4e46f9b24ede346c22e737be4a
Kristófer R <kristofer.thorlaksson@automattic.com>
2020-01-17 23:15:59 +0000

    Add reading time to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import readingTime from 'reading-time'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
@@ -8,13 +9,17 @@ import './blog-post.css';
 const BlogPostTemplate = ({ data }) => {
     const postData = data.wordpressPost
     //const featuredImg = postData.featured_media ? postData.featured_media.localFile.childImageSharp.fixed : undefined;
+    const stats = readingTime( postData.content );
 
     return (
         <Layout title={'thoughts on things'}>
             <SEO title={postData.title} description={postData.excerpt} />
 			<div className={ 'post-header' } >
 				<h1 dangerouslySetInnerHTML={{ __html: postData.title }} />
-				<p className={ 'post-meta' }>{ postData.date }</p>
+				<div className={ 'post-meta' }>
+        				<p>&#9719; { stats.text }</p>
+        				<p>{ postData.date }</p>
+				</div>
 			</div>
             <div className={ 'entry-content' } dangerouslySetInnerHTML={{ __html: postData.content }} />
         </Layout>
