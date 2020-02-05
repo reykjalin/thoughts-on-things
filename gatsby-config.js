f3843f47cf96f23d1bb8db96c1c684c56df4c672
Kristófer R <kristofer.thorlaksson@automattic.com>
2020-02-05 19:20:03 +0000

    Add syntax highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,72 +1,82 @@
+require( 'dotenv' ).config( {
+	path: `.env.${ process.env.NODE_ENV }`,
+} );
+
 module.exports = {
-    siteMetadata: {
-        title: `thoughts on things`,
-        author: `Kristófer Reykjalín Þorláksson`,
-        description: `A collection of thoughts on various things.`,
-        siteUrl: `https://thorlaksson.com/`,
-        social: {
-            twitter: `kreykjalin`,
+	siteMetadata: {
+		title: `thoughts on things`,
+		author: `Kristófer Reykjalín Þorláksson`,
+		description: `A collection of thoughts on various things.`,
+		siteUrl: `https://thorlaksson.com/`,
+		social: {
+			twitter: `kreykjalin`,
 			github: `reykjalin`,
 			keybase: `reykjalin`,
 			email: `kristofer@thorlaksson.com`
-        },
-    },
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/content/assets`,
-                name: `assets`,
-            },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
-            options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 590,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    `gatsby-remark-prismjs`,
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`,
-                    {
-                        resolve: `gatsby-source-wordpress`,
-                        options: {
-                            baseUrl: `thoughts.thorlaksson.com`,
-                            protocol: `https`,
-                            hostingWPCOM: false,
-                            useACF: false,
-                            verboseOutput: false,
-                            perPage: 100,
-                            searchAndReplaceContentUrls: {
-                                sourceUrl: `https://kreykjalin.wpcomstaging.com`,
-                                replacementUrl: `https://thorlaksson.com`,
-                            },
-                            concurrentRequests: 10,
-                            includedRoutes: [
-                                `**/categories`,
-                                `**/posts`,
-                                `**/pages`,
-                                `**/media`,
-                                `**/tags`,
-                                `**/taxonomies`,
-                                `**/users`,
-                            ],
-                        },
-                    },
-                ],
-            },
-        },
-        `gatsby-transformer-sharp`,
+		},
+	},
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/assets`,
+				name: `assets`,
+			},
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-images`,
+						options: {
+							maxWidth: 590,
+						},
+					},
+					{
+						resolve: `gatsby-remark-responsive-iframe`,
+						options: {
+							wrapperStyle: `margin-bottom: 1.0725rem`,
+						},
+					},
+					`gatsby-remark-prismjs`,
+					`gatsby-remark-copy-linked-files`,
+					`gatsby-remark-smartypants`,
+					{
+						resolve: `gatsby-source-wordpress`,
+						options: {
+							baseUrl: `thoughts.thorlaksson.com`,
+							protocol: `https`,
+							hostingWPCOM: true,
+							auth: {
+								wpcom_app_clientSecret: process.env.WP_CLIENT_SECRET,
+								wpcom_app_clientId: process.env.WP_CLIENT_ID,
+								wpcom_user: process.env.WP_USER,
+								wpcom_pass: process.env.WP_PASS,
+							},
+							useACF: false,
+							verboseOutput: false,
+							perPage: 100,
+							searchAndReplaceContentUrls: {
+								sourceUrl: `https://kreykjalin.wpcomstaging.com`,
+								replacementUrl: `https://thorlaksson.com`,
+							},
+							concurrentRequests: 10,
+							includedRoutes: [
+								`**/categories`,
+								`**/posts`,
+								`**/pages`,
+								`**/media`,
+								`**/tags`,
+								`**/taxonomies`,
+								`**/users`,
+							],
+						},
+					},
+				],
+			},
+		},
+		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
 		{
 			resolve: `gatsby-plugin-feed`,
@@ -117,26 +127,26 @@ module.exports = {
 				],
 			},
 		},
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `thoughts on things`,
-                short_name: `thoughts on things`,
-                start_url: `/`,
-                background_color: `#2f3640`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `thoughts on things`,
+				short_name: `thoughts on things`,
+				start_url: `/`,
+				background_color: `#2f3640`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
 				icon: `content/assets/gatsby-icon.png`,
 				include_favicon: false,
-            },
-        },
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography`,
-            },
-        },
-    ],
+			},
+		},
+		`gatsby-plugin-offline`,
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-plugin-typography`,
+			options: {
+				pathToConfigModule: `src/utils/typography`,
+			},
+		},
+	],
 }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,44 +2,81 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import readingTime from 'reading-time'
 
+import parse, { domToReact } from 'html-react-parser';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import './blog-post.css';
 
-const BlogPostTemplate = ({ data }) => {
-    const postData = data.wordpressPost;
-    //const featuredImg = postData.featured_media ? postData.featured_media.localFile.childImageSharp.fixed : undefined;
-    const stats = readingTime( postData.content );
+const PostCode = ( { language, children } ) => (
+	<SyntaxHighlighter
+		language={ language }
+		style={ a11yLight }>
+		{ children }
+	</SyntaxHighlighter>
+);
 
-    return (
-        <Layout title={'thoughts on things'}>
-            <SEO title={postData.title} description={postData.excerpt} />
+const getLanguage = node => {
+	// language custom class is the last class.
+	return node.attribs.class.split( ' ' ).pop();
+}
+
+const getCode = node => {
+	if ( node.children.length > 0 && 'code' === node.children[0].name) {
+		return node.children[0].children;
+	}
+	return node.children;
+}
+
+const replaceCode = node => {
+	if ( 'pre' !== node.name || node.children.length <= 0 ) {
+		return;
+	}
+	return (
+		<PostCode language={ getLanguage( node ) }>
+			{ domToReact( getCode( node ) ) }
+		</PostCode>
+	);
+}
+
+const BlogPostTemplate = ({ data }) => {
+	const postData = data.wordpressPost;
+	//const featuredImg = postData.featured_media ? postData.featured_media.localFile.childImageSharp.fixed : undefined;
+	const stats = readingTime( postData.content );
+
+	return (
+		<Layout title={'thoughts on things'}>
+			<SEO title={postData.title} description={postData.excerpt} />
 			<div className={ 'post-header' } >
 				<h1 dangerouslySetInnerHTML={{ __html: postData.title }} />
 				<div style={{ color: '#9fa7ac' }} dangerouslySetInnerHTML={{ __html: postData.excerpt }} />
 				<div className={ 'post-meta' }>
-        				<p>&#9719; { stats.text }</p>
-        				<p>{ postData.date }</p>
+						<p>&#9719; { stats.text }</p>
+						<p>{ postData.date }</p>
 				</div>
 			</div>
-            <div className={ 'entry-content' } dangerouslySetInnerHTML={{ __html: postData.content }} />
-        </Layout>
-    )
+			<div className={ 'entry-content' }>
+				{ parse( postData.content, { replace: replaceCode } ) }
+			</div>
+		</Layout>
+	)
 }
 
 export default BlogPostTemplate
 
 export const pageQuery = graphql`
-    query($id: Int!) {
-        wordpressPost(wordpress_id: { eq: $id }) {
-            title
-            content
-            excerpt
-            date(formatString: "YYYY-MM-DD")
-            author {
-                name
-            }
-            jetpack_featured_media_url
-        }
-    }
+	query($id: Int!) {
+		wordpressPost(wordpress_id: { eq: $id }) {
+			title
+			content
+			excerpt
+			date(formatString: "YYYY-MM-DD")
+			author {
+				name
+			}
+			jetpack_featured_media_url
+		}
+	}
 `
