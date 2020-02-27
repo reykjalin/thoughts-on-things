51acb740f75febf032594aa3aaab2ce2a152fc4d
Kristófer R <kristofer.thorlaksson@automattic.com>
2020-02-28 00:20:16 +0800

    Fix navigatoin analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,23 @@
 //import "typeface-montserrat"
 //import "typeface-merriweather"
 import './src/styles/global.css';
+
+export const onRouteUpdate = ( { location, prevLocation } ) => {
+	// Make sure stats are tracked correctly.
+	// Timeout is necessary to make sure script is loaded.
+	setTimeout( () => {
+		if ( prevLocation ) {
+			window.goatcounter.count( {
+				path: location.pathname,
+				referrer: prevLocation.href,
+				title: document.title,
+			} );
+		} else {
+			window.goatcounter.count( {
+				path: location.pathname,
+				referrer: document.referrer || null,
+				title: document.title,
+			} );
+		}
+	}, 400 );
+}
