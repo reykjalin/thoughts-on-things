0d9c2727a61af7ed0efa76e25f8bf56105ee07d0
Kristófer R <kristofer.thorlaksson@automattic.com>
2020-02-28 00:47:12 +0800

    Refactor onRouteUpdate

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,24 +1,20 @@
-// custom typefaces
-//import "typeface-montserrat"
-//import "typeface-merriweather"
+// Use global stylesheet.
 import './src/styles/global.css';
+
+const updateCount = ( path, referrer, title ) => {
+	window.goatcounter.count( {
+		path,
+		referrer: referrer || null,
+		title,
+	} );
+};
 
 export const onRouteUpdate = ( { location, prevLocation } ) => {
 	// Make sure stats are tracked correctly.
-	// Timeout is necessary to make sure script is loaded.
-	setTimeout( () => {
-		if ( prevLocation ) {
-			window.goatcounter.count( {
-				path: location.pathname,
-				referrer: prevLocation.href,
-				title: document.title,
-			} );
-		} else {
-			window.goatcounter.count( {
-				path: location.pathname,
-				referrer: document.referrer || null,
-				title: document.title,
-			} );
-		}
-	}, 400 );
-}
+	// Timeout is necessary to make sure GoatCounter script is loaded.
+	if ( prevLocation ) {
+		setTimeout( updateCount( location.pathname, prevLocation.href, document.title ), 400 );
+	} else {
+		setTimeout( updateCount( location.pathname, document.referrer, document.title ), 400 );
+	}
+};
